ad6dff882f26ed13df5bd2a7c700a9320806813d
Skakarla2023 <skakarla2023@gmail.com>
2025-06-24 21:44:52 +0530

    Create code.py

---
+++ Python-Basics/code.py
@@ -0,0 +1,59 @@
+# variables in python
+a=3
+b=4
+print(a+b)
+
+
+# declaring variables
+
+# camel-case
+myVar=4
+print(myVar)
+
+# snake-case
+my_var=5
+print(my_var)
+
+# Pascal case
+MyVar=4
+print(MyVar)
+
+
+# if in python
+if(5>2):
+  print("5 is greater than 2")
+else:
+  print("5 is not greater than 2")
+
+
+# declaring multiple variables is easy
+fruits="apple","banana","cherry"
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
+
+
+# global variables
+x="awesome"
+
+def myfunc():
+  x="fantastic"
+  print("python is",x)
+
+myfunc()
+print("python is",x)
+
+
+# changing the value of global variables
+
+x="awesome"
+
+def myfunc():
+  global x
+  x="fantastic"
+  print("python is",x)
+
+myfunc()
+print("python is",x)
+
