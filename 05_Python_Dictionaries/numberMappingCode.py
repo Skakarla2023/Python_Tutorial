944287520b28aa198bb386eccdab2487779ae355
Skakarla2023 <skakarla2023@gmail.com>
2025-08-12 05:24:28 +0530

    Create numberMappingCode.py

---
+++ 05_Python_Dictionaries/numberMappingCode.py
@@ -0,0 +1,19 @@
+num=input("Phone: ")
+keys={
+    "1":"one",
+    "2":"two",
+    "3":"three",
+    "4":"four",
+    "5":"five",
+    "6":"six",
+    "7":"seven",
+    "8":"eight",
+    "9":"nine",
+    "0":"zero"
+}
+
+output=""
+for ch in num:
+    output+=keys.get(ch,"!")+" "
+
+print(output)
